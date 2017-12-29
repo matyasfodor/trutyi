7490f79142bcd11c8628873e85f852ef6f693438
Mátyás Fodor <matyas.fodor@gmail.com>
2017-12-29 11:08:07 +0100

    Use client token

--- trutyiExtension/src/bg/background.js
+++ trutyiExtension/src/bg/background.js
@@ -4,24 +4,32 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
+const backend = 'https://enigmatic-savannah-14867.herokuapp.com';
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+    if (request.from == 'content' && request.action == 'show_page_action') {
+      chrome.pageAction.show(sender.tab.id);
+      sendResponse();
+    } else if (request.from == 'content' && request.action == 'add_term') {
+      var payload = request.payload;
+      payload.access_token = localStorage.getItem('quizlet_access_token');
+      fetch('https://enigmatic-savannah-14867.herokuapp.com/terms/', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+      }).then(() => {sendResponse('OK');}, () => {sendResponse('NOK');})
+    }
   });
 
 // TODO IIFE, strict mode;
 var quizletWindow;
 document.getElementById('quizlet-login').onclick = () => {
-  var url = 'https://enigmatic-savannah-14867.herokuapp.com/auth/';
+  var url = backend + '/auth/';
   quizletWindow = window.open(url, 'Quizlet auth', "height=600,width=1100");
 };
 
 window.addEventListener("message", function(message) {
-  let {code} = message.data;
+  let {access_token} = message.data;
   quizletWindow.close();
-  localStorage.setItem('myCat', 'Tom');
-  console.log('message received: ', arguments)
+  localStorage.setItem('quizlet_access_token', access_token);
 }, false);
--- trutyiExtension/src/inject/inject.js
+++ trutyiExtension/src/inject/inject.js
@@ -1,41 +1,42 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({
+  from: 'content',
+  action: 'show_page_action',
+}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+    // ----------------------------------------------------------
+    // This part of the script triggers when page is done loading
+    console.log("Hello. This message was sent from scripts/inject.js");
+    // ----------------------------------------------------------
 
-	}
-	}, 10);
+  }
+  }, 10);
 });
 
 function string2HTML(string) {
-	var div = document.createElement('div');
-	div.innerHTML = string;
-	return div.firstChild;
+  var div = document.createElement('div');
+  div.innerHTML = string;
+  return div.firstChild;
 }
 
 var parent = document.getElementById('gt-res-tools-l');
 
-// div.innerHTML = `<div id="gt-res-cucucu" class="share-button goog-toolbar-button"style="user-select: none;">
-// 		<span style="font-weight: 600">TRUTYI</span>
-// </div>`;
 element = string2HTML(`<button style="font-weight: 600">TRUTYI</button>`);
 element.addEventListener('click', function(e) {
-	e.preventDefault();
-	var payload = {
-		term: document.getElementById('source').value,
-		definition: document.getElementById('result_box').textContent,
-	};
-
-	fetch('https://enigmatic-savannah-14867.herokuapp.com/terms/', {
-		method: 'POST',
-		body: JSON.stringify(payload),
-	}).then(() => {
-	}, (a) => {
-	})
+  e.preventDefault();
+  var payload = {
+    term: document.getElementById('source').value,
+    definition: document.getElementById('result_box').textContent,
+  };
+  chrome.runtime.sendMessage({
+    from: 'content',
+    action: 'add_term',
+    payload: payload,
+  }, function(response){
+    console.log(response);
+    //If you need a response, do stuff with it here
+  });
 });
 parent.appendChild(element);
