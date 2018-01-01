96fab2e628bd5c916d4b5c305a79f126544b6999
Mátyás Fodor <matyas.fodor@gmail.com>
2018-01-01 10:16:17 +0100

    secure django app

--- trutyi/settings.py
+++ trutyi/settings.py
@@ -20,10 +20,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "0*n&j)-6!ekqzyjpg8g87!&ec)rs1h484qez0_q9bg4q+4002_"
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
